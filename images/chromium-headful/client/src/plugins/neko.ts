4636ad2960981e23a5220cb675034b5b3fe0c644
Archan Datta <archandatta@lazertechnologies.com>
2026-02-25 11:43:32 -0400

    feat: update neko client to have optional debugging

--- images/chromium-headful/client/src/plugins/neko.ts
+++ images/chromium-headful/client/src/plugins/neko.ts
@@ -15,13 +15,22 @@ declare module 'vue/types/vue' {
   }
 }
 
+function isDebugEnabled(): boolean {
+  const params = new URLSearchParams(location.search)
+  const debug = params.get('debug')
+  return debug === '1' || debug === 'true'
+}
+
 const plugin: PluginObject<undefined> = {
   install(Vue) {
-    window.$client = new NekoClient()
+    const client = new NekoClient()
       .on('error', window.$log.error)
       .on('warn', window.$log.warn)
       .on('info', window.$log.info)
-      .on('debug', window.$log.debug)
+    if (isDebugEnabled()) {
+      client.on('debug', window.$log.debug)
+    }
+    window.$client = client
 
     Vue.prototype.$client = window.$client
   },
